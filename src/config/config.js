020b724486b09b101757005c614f4094180c2cd7
Bryan Jackson <bjack13@gmail.com>
2019-08-27 22:26:39 -0600

    React Most Wanted (RMW) initial setup

--- src/config/config.js
+++ src/config/config.js
@@ -16,22 +16,22 @@ const LPAsync = Loadable({
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650',
-    appId: '1:258373383650:web:b49ad5dd28da999a'
+    apiKey: "AIzaSyBFbCYD5GFoSRMQnbALpG0FrhGLsgNDJww",
+    authDomain: "starsightlive.firebaseapp.com",
+    databaseURL: "https://starsightlive.firebaseio.com",
+    projectId: "starsightlive",
+    storageBucket: "",
+    messagingSenderId: "534699431997",
+    appId: "1:534699431997:web:461d4b5cb1503a46"
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824',
-    appId: '1:70650394824:web:7cd3113c37741efc'
+    apiKey: "AIzaSyAHDWj-zMh0Nejbz1I2X2HrkLGmU-6xuSA",
+    authDomain: "starsightdev.firebaseapp.com",
+    databaseURL: "https://starsightdev.firebaseio.com",
+    projectId: "starsightdev",
+    storageBucket: "",
+    messagingSenderId: "301663324805",
+    appId: "1:301663324805:web:4dedb46d9cf3d077"
   },
   firebase_providers: ['google.com', 'facebook.com', 'twitter.com', 'github.com', 'password', 'phone'],
   initial_state: {
